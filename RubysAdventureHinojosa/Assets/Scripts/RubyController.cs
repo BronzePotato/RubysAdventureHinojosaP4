f30e3bd6886ddd74ef0e47c663de4102f7af8595
BronzePotato <BronzeVulture101@gmail.com>
2022-12-06 11:45:39 -0800

    Ruby Replacement for Animation, Projectile Addition
    Replace Previous Ruby with Prefab for animation, added Cog for Projectile, new Projectile Script, and added code to Ruby Controller

--- RubysAdventureHinojosa/Assets/Scripts/RubyController.cs
+++ RubysAdventureHinojosa/Assets/Scripts/RubyController.cs
@@ -53,6 +53,11 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
 
     void FixedUpdate()
@@ -79,4 +84,13 @@ public class RubyController : MonoBehaviour
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefabs, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+    }
 }
